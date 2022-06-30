684c61bc6a6974e6491e997f54e973eb43941332
peve <paulovictor237@gmail.com>
2022-06-29 23:44:22 -0300

    estilo padrao

--- aula-01/src/App.jsx
+++ aula-01/src/App.jsx
@@ -1,10 +1,12 @@
+import Header from "../components/Header"
+import './global.css'
 import Post from "./Post"
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
       <Post
         author="peve"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga odit consectetur inventore, repellat ducimus delectus eveniet corrupti est nihil facilis, error dolorem ipsam debitis assumenda labore laudantium sint! Quas, vero."
